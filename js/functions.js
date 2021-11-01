f9d9a570668092a9b23390ad285f8c742289f506
dev-mapify <dev.mapify@htmlbox.net>
2021-11-01 10:01:19 +0200

    Fix issue with multiple banners

--- js/functions.js
+++ js/functions.js
@@ -13,175 +13,178 @@
 		var $container = $('.bar-plugin');
 		
 		if($container.length){
+			$container.each(function(index, elem) {
+				var $elem = $(elem);
 
-			if($container.data('show') == true){
-				var _delay = $container.data('delay');
-				var _position = $container.data('position');
-				var _btnTopGradient = $container.data('button-gradient-top');
-				var _btnBottomGradient = $container.data('button-gradient-bottom');
-				var _backgroundTopGradient = $container.data('background-gradient-top');
-				var _backgroundBottomGradient = $container.data('background-gradient-bottom');
-				var _color = $container.data('color');
-				var _separator = $container.data('separator');
-				var _prefix = 'bar-plugin';
-				var $classes = {
-					ShowBar : _prefix + '-show-bar'
-				}
-				var _height = 0;
-				var _fixedHeaderSelector = $container.data('fixed-header');
-				var $fixedHeader = $(_fixedHeaderSelector);
-
-				$container.css({
-					'background' : 'linear-gradient(' + _backgroundTopGradient + ', ' + _backgroundBottomGradient +')',
-					'color' : _color
-				});
-
-				$container.find('.' + _prefix + '-btn').css({
-					'background' : 'linear-gradient(' + _btnTopGradient + ', ' + _btnBottomGradient +')'
-				});
-
-				$container.find('.' + _prefix + '-separator').css({
-					'background' : _separator
-				});
-
-				var _clone = $container.clone();
-
-				$container.remove();
-
-				if (_position == 'bottom') {
-					_clone.appendTo('body');
-				} else {
-					_clone.prependTo('body');
-				}
-
-				var $barPlugin = $body.find('.' + _prefix + ' ul');
-
-				$body.find('.' + _prefix + '-btn-close, .sa-close-btn').click(function(event){
-					var $this = $(this);
-					var $bar = $this.closest('.bar-plugin');
-
-					setTimeout(function() {
-						if ($fixedHeader.length > 0 && $fixedHeader.css('position') === 'fixed' || $fixedHeader.css('position') === 'absolute') {
-							$fixedHeader.animate({
-								'margin-top' : '0'
-							});
-
-							$body.animate({
-								'margin-top' : '0'
-							});
-						}
-					}, _delay)
-
-					event.preventDefault();
-					$body.find('.' + _prefix).removeClass($classes.ShowBar);
-
-					var snoozeBar = $bar.attr('data-snooze');
-					var snoozeDays = $bar.attr('data-snooze-days');
-					var id = $bar.attr('data-id');
-					snoozeDays = parseInt(snoozeDays);
-
-					if (snoozeBar && snoozeDays) {
-						mb_setCookie('crb_sa_hide_banner_' + id, true, snoozeDays);
+				if ($elem.data('show') == true){
+					var _delay = $elem.data('delay');
+					var _position = $elem.data('position');
+					var _btnTopGradient = $elem.data('button-gradient-top');
+					var _btnBottomGradient = $elem.data('button-gradient-bottom');
+					var _backgroundTopGradient = $elem.data('background-gradient-top');
+					var _backgroundBottomGradient = $elem.data('background-gradient-bottom');
+					var _color = $elem.data('color');
+					var _separator = $elem.data('separator');
+					var _prefix = 'bar-plugin';
+					var $classes = {
+						ShowBar : _prefix + '-show-bar'
 					}
-				});
+					var _height = 0;
+					var _fixedHeaderSelector = $elem.data('fixed-header');
+					var $fixedHeader = $(_fixedHeaderSelector);
 
-				$win.load(function(){
-					_height = $barPlugin.height();
-					_data_height = parseInt($barPlugin.closest('.bar-plugin').data('height'));
-					
-					if (_data_height > _height) {
-						$barPlugin.css('height', _data_height);
+					$elem.css({
+						'background' : 'linear-gradient(' + _backgroundTopGradient + ', ' + _backgroundBottomGradient +')',
+						'color' : _color
+					});
+
+					$elem.find('.' + _prefix + '-btn').css({
+						'background' : 'linear-gradient(' + _btnTopGradient + ', ' + _btnBottomGradient +')'
+					});
+
+					$elem.find('.' + _prefix + '-separator').css({
+						'background' : _separator
+					});
+
+					var _clone = $elem.clone();
+
+					$elem.remove();
+
+					if (_position == 'bottom') {
+						_clone.appendTo('body');
 					} else {
-						$barPlugin.css('height', _height);
+						_clone.prependTo('body');
 					}
 
-					if ($fixedHeader.length > 0 && $fixedHeader.css('position') === 'fixed') {
-						var _top = $body.hasClass('admin-bar') ? $('#wpadminbar') .outerHeight() : '0';
+					var $barPlugin = $body.find('.' + _prefix + ' ul');
 
-						$barPlugin.closest('.bar-plugin').css({
-							'position' : 'fixed',
-							'top'      : _top,
-							'left'      : '0',
-							'right'      : '0'
-						});
-					} else if ($fixedHeader.length > 0 && $fixedHeader.css('position') === 'absolute') {
-						var _top = $body.hasClass('admin-bar') ? $('#wpadminbar') .outerHeight() : '0';
+					$body.find('.' + _prefix + '-btn-close, .sa-close-btn').click(function(event){
+						var $this = $(this);
+						var $bar = $this.closest('.bar-plugin');
 
-						$barPlugin.closest('.bar-plugin').css({
-							'position' : 'absolute',
-							'top'      : _top,
-							'left'      : '0',
-							'right'      : '0'
-						});
-					}
+						setTimeout(function() {
+							if ($fixedHeader.length > 0 && $fixedHeader.css('position') === 'fixed' || $fixedHeader.css('position') === 'absolute') {
+								$fixedHeader.animate({
+									'margin-top' : '0'
+								});
 
-					setTimeout(function() {
-						$body.find('.' + _prefix).addClass($classes.ShowBar);
+								$body.animate({
+									'margin-top' : '0'
+								});
+							}
+						}, _delay)
 
-						if ($fixedHeader.length > 0 && $fixedHeader.css('position') === 'fixed' || $fixedHeader.css('position') === 'absolute') {
-							$fixedHeader.animate({
-								'margin-top' : $barPlugin.css('height')
+						event.preventDefault();
+						$body.find('.' + _prefix).removeClass($classes.ShowBar);
+
+						var snoozeBar = $bar.attr('data-snooze');
+						var snoozeDays = $bar.attr('data-snooze-days');
+						var id = $bar.attr('data-id');
+						snoozeDays = parseInt(snoozeDays);
+
+						if (snoozeBar && snoozeDays) {
+							mb_setCookie('crb_sa_hide_banner_' + id, true, snoozeDays);
+						}
+					});
+
+					$win.load(function(){
+						_height = $barPlugin.height();
+						_data_height = parseInt($barPlugin.closest('.bar-plugin').data('height'));
+
+						if (_data_height > _height) {
+							$barPlugin.css('height', _data_height);
+						} else {
+							$barPlugin.css('height', _height);
+						}
+
+						if ($fixedHeader.length > 0 && $fixedHeader.css('position') === 'fixed') {
+							var _top = $body.hasClass('admin-bar') ? $('#wpadminbar') .outerHeight() : '0';
+
+							$barPlugin.closest('.bar-plugin:not(.bottom)').css({
+								'position' : 'fixed',
+								'top'      : _top,
+								'left'      : '0',
+								'right'      : '0'
 							});
+						} else if ($fixedHeader.length > 0 && $fixedHeader.css('position') === 'absolute') {
+							var _top = $body.hasClass('admin-bar') ? $('#wpadminbar') .outerHeight() : '0';
 
-							$body.animate({
-								'margin-top' : $barPlugin.css('height')
+							$barPlugin.closest('.bar-plugin:not(.bottom)').css({
+								'position' : 'absolute',
+								'top'      : _top,
+								'left'      : '0',
+								'right'      : '0'
 							});
 						}
 
-						$win.on('resize', function(){
-							$barPlugin.removeAttr('style');
-
-							_height = $barPlugin.height();
-							_data_height = parseInt($barPlugin.closest('.bar-plugin').data('height'));
-							_used_height = 0;
-
-							if (_data_height > _height) {
-								_used_height = _data_height;
-							} else {
-								_used_height = _height;
-							}
-
-							$barPlugin.css('height', _used_height);
+						setTimeout(function() {
+							$body.find('.' + _prefix).addClass($classes.ShowBar);
 
 							if ($fixedHeader.length > 0 && $fixedHeader.css('position') === 'fixed' || $fixedHeader.css('position') === 'absolute') {
-								var _top = $body.hasClass('admin-bar') ? $('#wpadminbar') .outerHeight() : '0';
+								$fixedHeader.animate({
+									'margin-top' : $barPlugin.css('height')
+								});
 
-								$barPlugin.closest('.bar-plugin').css({
-									'top'        : _top,
+								$body.animate({
+									'margin-top' : $barPlugin.css('height')
 								});
 							}
 
-							if ($fixedHeader.length && $fixedHeader.css('position') === 'fixed') {
-								$barPlugin.closest('.bar-plugin').css({
-									'top'             : _top,
-									'position'        : 'fixed',
-								});
+							$win.on('resize', function(){
+								$barPlugin.removeAttr('style');
 
-								$fixedHeader.css({
-									'margin-top' : _used_height
-								});
+								_height = $barPlugin.height();
+								_data_height = parseInt($barPlugin.closest('.bar-plugin').data('height'));
+								_used_height = 0;
 
-								$body.css({
-									'margin-top' : _used_height
-								});
-							} else if ($fixedHeader.length > 0 && $fixedHeader.css('position') === 'absolute') {
-								$barPlugin.closest('.bar-plugin').css({
-									'top'             : _top,
-									'position'        : 'absolute',
-								});
-							} else if ($fixedHeader.length > 0 && $fixedHeader.css('position') === 'static') {
-								$barPlugin.closest('.bar-plugin').css({
-									'position'        : 'static',
-								});
-							}
-						});
+								if (_data_height > _height) {
+									_used_height = _data_height;
+								} else {
+									_used_height = _height;
+								}
 
-					}, _delay);
+								$barPlugin.css('height', _used_height);
 
-				});
-			}else{
-				$container.remove();
-			}
+								if ($fixedHeader.length > 0 && $fixedHeader.css('position') === 'fixed' || $fixedHeader.css('position') === 'absolute') {
+									var _top = $body.hasClass('admin-bar') ? $('#wpadminbar') .outerHeight() : '0';
+
+									$barPlugin.closest('.bar-plugin:not(.bottom)').css({
+										'top'        : _top,
+									});
+								}
+
+								if ($fixedHeader.length && $fixedHeader.css('position') === 'fixed') {
+									$barPlugin.closest('.bar-plugin:not(.bottom)').css({
+										'top'             : _top,
+										'position'        : 'fixed',
+									});
+
+									$fixedHeader.css({
+										'margin-top' : _used_height
+									});
+
+									$body.css({
+										'margin-top' : _used_height
+									});
+								} else if ($fixedHeader.length > 0 && $fixedHeader.css('position') === 'absolute') {
+									$barPlugin.closest('.bar-plugin:not(.bottom)').css({
+										'top'             : _top,
+										'position'        : 'absolute',
+									});
+								} else if ($fixedHeader.length > 0 && $fixedHeader.css('position') === 'static') {
+									$barPlugin.closest('.bar-plugin:not(.bottom)').css({
+										'position'        : 'static',
+									});
+								}
+							});
+
+						}, _delay);
+
+					});
+				} else{
+					$elem.remove();
+				}
+			});
 		}
 	}
 
